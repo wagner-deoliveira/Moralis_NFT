0725ab0c7743295e14116788143b7087698d954b
Wagner Rosa <wagner.deoliveira@perkinelmer.com>
2021-04-02 22:30:39 +0200

    Raname Tokens to Token. Deploy NFT contract

--- migrations/2_token_migration.js
+++ migrations/2_token_migration.js
@@ -1,7 +1,9 @@
-const Token = artifacts.require("Migrations");
+const Token = artifacts.require("Token");
 
-module.exports = function (deployer) {
-  await deployer.deploy(Token);
-  let tokenInstance = await Tokern.deployed();
+module.exports = async function(deployer) {
+  await deployer.deploy(Token, "NFT Game", "NFTG");
+  let tokenInstance = await Token.deployed();
   await tokenInstance.mint(100, 200, 100000); // Token id 0
+  let pet = await tokenInstance.getTokenDetails(0);
+  console.log(pet);
 };
